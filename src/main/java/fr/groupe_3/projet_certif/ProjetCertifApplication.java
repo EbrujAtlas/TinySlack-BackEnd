cea21be39d76f341cfaf4c71e4925a516f02f5cd
Ebru Jolivet <ebruozogul1@gmail.com>
2023-08-01 11:14:04 +0200

    creation repo

---
+++ src/main/java/fr/groupe_3/projet_certif/ProjetCertifApplication.java
@@ -0,0 +1,13 @@
+package fr.groupe_3.projet_certif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCertifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCertifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/groupe_3/projet_certif/ProjetCertifApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.groupe_3.projet_certif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCertifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
