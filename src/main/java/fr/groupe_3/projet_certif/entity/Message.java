a60a2bfddbb92de9013f9a114f2f595148563e79
Ebru Jolivet <ebruozogul1@gmail.com>
2023-08-01 14:21:43 +0200

    creation message terminée

--- src/main/java/fr/groupe_3/projet_certif/entity/Message.java
+++ src/main/java/fr/groupe_3/projet_certif/entity/Message.java
@@ -1,5 +1,125 @@
 package fr.groupe_3.projet_certif.entity;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+
+    // Attributs
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID idMessage;
+
+    @Column(name = "message")
+    private String messageContent;
+
+    @Column(name = "date_message")
+    private LocalDateTime messageDate;
+
+    @Column(name = "nom_utilisateur")
+    private String userName;
+
+    @ManyToOne
+    @Column(name = "canal_id")
+    private Canal canal;
+
+    // Constructeurs
+    public Message(String messageContent, LocalDateTime messageDate, String userName, Canal canal) {
+        this.messageContent = messageContent;
+        this.messageDate = messageDate;
+        this.userName = userName;
+        this.canal = canal;
+    }
+
+    // Constructeur vide
+
+    public UUID getIdMessage() {
+        return idMessage;
+    }
+
+    // Getters
+
+    public Message() {
+    }
+
+    public String getMessageContent() {
+        return messageContent;
+    }
+
+    public LocalDateTime getMessageDate() {
+        return messageDate;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Canal getCanal() {
+        return canal;
+    }
+
+    // Setters
+    public void setIdMessage(UUID idMessage) {
+        this.idMessage = idMessage;
+    }
+
+    public void setMessageContent(String messageContent) {
+        this.messageContent = messageContent;
+    }
+
+    public void setMessageDate(LocalDateTime messageDate) {
+        this.messageDate = messageDate;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setCanal(Canal canal) {
+        this.canal = canal;
+    }
+
+    // To String
+    @Override
+    public String toString() {
+        return "Message [idMessage=" + idMessage + ", messageContent=" + messageContent + ", messageDate=" + messageDate
+                + ", userName=" + userName + ", canal=" + canal + "]";
+    }
+
+    // Méthode updateNotNull pour le PATCH
+
+    public void updateNotNull(Message messagePatch) {
+
+        // messageContent
+        if (messagePatch.getMessageContent() != null) {
+            this.setMessageContent(messagePatch.getMessageContent());
+        }
+
+        // MessageDate
+        if (messagePatch.getMessageDate() != null) {
+            this.setMessageDate(messagePatch.getMessageDate());
+        }
+
+        // userName
+        if (messagePatch.getUserName() != null) {
+            this.setUserName(messagePatch.getUserName());
+        }
+
+        // canal
+        if (messagePatch.getCanal() != null) {
+            this.setCanal(messagePatch.getCanal());
+        }
+
+    }
 
 }
--- src/main/java/fr/groupe_3/projet_certif/service/MessageService.java
+++ src/main/java/fr/groupe_3/projet_certif/service/MessageService.java
@@ -1,5 +1,68 @@
 package fr.groupe_3.projet_certif.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.groupe_3.projet_certif.dao.MessageRepository;
+import fr.groupe_3.projet_certif.entity.Message;
+
+@Service
 public class MessageService {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    // Get all
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    /**
+     * 
+     * @param idMessage
+     * @return
+     */
+    // Get one
+    public Optional<Message> getOneMessageById(UUID idMessage) {
+        return messageRepository.findById(idMessage);
+    }
+
+    /**
+     * 
+     * @param idMessage
+     */
+    // Delete one
+    public void deleteById(UUID idMessage) {
+        messageRepository.deleteById(idMessage);
+    }
+
+    // Post one
+    public Message addMessage(Message message) {
+        return messageRepository.save(message);
+    }
+
+    // Put
+    public void updatedMessage(UUID idMessage, Message message) {
+        messageRepository.save(message);
+    }
+
+    // Patch
+    public void patchMessage(UUID idMessage, Message messagePatch) {
+
+        Optional<Message> optional = messageRepository.findById(idMessage);
+
+        if (optional.isPresent()) {
+
+            Message message = optional.get();
+            System.out.println(message);
+            message.updateNotNull(messagePatch);
+            messageRepository.save(message);
+
+        }
+    }
 
 }
